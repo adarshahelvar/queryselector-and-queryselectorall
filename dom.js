1baef19d041980a9a5fb7ba9b916b637b1e02638
Adarsha A <108178521+adarshahelvar@users.noreply.github.com>
2022-06-27 20:24:39 +0530

    Update dom.js

--- dom.js
+++ dom.js
@@ -1,1 +1,15 @@
+var submit = document.querySelector('input[type="submit"]');
+submit.value="send";
+
+var items=document.querySelector('.list-group-item:nth-child(2)');
+items.style.backgroundColor ="red";
+var items=document.querySelector('.list-group-item:nth-child(3)');
+items.style.visibility = "hidden";
+
+//QuerySelectorALL
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+for(var i=0; i<odd.length; i++){
+    odd[i].style.backgroundColor = "green";
+}
 
